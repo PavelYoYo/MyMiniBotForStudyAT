2a14c979440c50a7001a3becc654ced6e5555d1d
PavelYoYo <ignate777@gmail.com>
2020-03-25 18:52:22 +0300

    initial commit

---
+++ ExampleMyProject/.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ExampleMyProject
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 91);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Поиск в Google";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(343, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Close Browser";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(252, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(344, 58);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Спсообы поиск элементов и клики";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(311, 197);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(208, 69);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "открыть браузер";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(316, 452);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(203, 37);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Перебор элементов";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.Location = new System.Drawing.Point(252, 336);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 95);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(343, 507);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(153, 70);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Выполнить JS";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 618);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ ExampleMyProject/Main.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ExampleMyProject
+{
+    public partial class Main : Form
+    {
+        IWebDriver BrowserPavelYO ;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BrowserPavelYO = new OpenQA.Selenium.Chrome.ChromeDriver();
+            BrowserPavelYO.Manage().Window.Maximize();
+            BrowserPavelYO.Navigate().GoToUrl("http://google.com");
+            IWebElement SearchInput = BrowserPavelYO.FindElement(By.Name ("q"));
+            SearchInput.SendKeys("с#" + OpenQA.Selenium.Keys.Enter);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BrowserPavelYO.Quit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IWebElement elementPavelYo;
+            //поиск по ID
+            elementPavelYo = BrowserPavelYO.FindElement(By.Id("text"));
+            elementPavelYo.SendKeys("тест");
+
+            //поиск по имени класса
+            elementPavelYo = BrowserPavelYO.FindElement(By.ClassName("football"));
+            elementPavelYo.Click();
+
+            //поиск по тексту ссылки
+            elementPavelYo = BrowserPavelYO.FindElement(By.LinkText("Картинки"));
+            elementPavelYo.Click();
+
+            //поиск по частичному тексту ссылки (переводчик)
+            elementPavelYo = BrowserPavelYO.FindElement(By.PartialLinkText("ревод"));
+            elementPavelYo.Click();
+
+            // Варинатов много .... ( мне удобнее по )
+        }
+
+        // открытие пустого браузера без параметров ( так же есть с параметрами )
+        private void button4_Click(object sender, EventArgs e)
+        {
+            BrowserPavelYO = new OpenQA.Selenium.Chrome.ChromeDriver();
+            BrowserPavelYO.Manage().Window.Maximize();
+        }
+
+        //  как в зависемости от текста сделать что то со строкой
+
+        /* 
+         * Шаги: 
+         * Открыть браузер 
+         * открыть главную старницу яндекса
+         * В зависемости от новости вбить нужный текст в шаги
+         */
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<IWebElement> News = BrowserPavelYO.FindElements(By.CssSelector("#tabnews_newsc a")).ToList();
+
+            for (int i = 0; i < News.Count; i++)
+            {
+                String s = News[i].Text; // текст новости с номером i
+
+                if (s.StartsWith(""))
+                {
+                    textBox1.AppendText("Информация №" + (i + 1).ToString() + "  начинается с 'Вирус'" + "\r\n");
+                }
+
+                if (s.EndsWith("Кремля"))
+                {
+                    textBox1.AppendText("Информация № " + (i + 1).ToString() + "  заканчивается  'домой'" + "\r\n");
+                }
+
+                if (s.Contains("вывести"))
+                {
+                    textBox1.AppendText("Информация №" + (i + 1).ToString() + "  содержит 'удалит из'" + "\r\n");
+                    News[i].Click();
+                    break;
+                }
+            }
+        }
+
+        // JavaScript  в автотестах 
+        private void button6_Click(object sender, EventArgs e)
+        {
+             IJavaScriptExecutor jse = BrowserPavelYO as IJavaScriptExecutor; // тип JS экзекютер
+            jse.ExecuteScript("alert('Закрой это окно');");
+        }
+
+        // текстовое окно
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
